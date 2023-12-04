975727c028fb4f3e46a0d6e8e5cc4e4402deaa33
chanakya_ex3 <chanakya_ex3@kali>
2023-12-04 15:21:45 +0530

    name written 2

--- src/App.js
+++ src/App.js
@@ -9,16 +9,12 @@ const App = () => {
   return (
     <>
       <div class="startHeader"></div>
-      <div class="container text-center head-box">
-        <div class="row align-items-start">
-          <div class="box name">RITIKA SAHANI</div>
-          <div class="col box gutter">
-          </div>
-          <div class="box name">
-            <div class="form-check form-switch">
+      <div class="head-box">
+        <div class="box name">RITIKA SAHANI</div>
+        <div class="box">
+        <div class="form-check form-switch">
               <input class="form-check-input switchc" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             </div>
-          </div>
         </div>
       </div>
     </>
